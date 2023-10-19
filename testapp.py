3bea92c908cf6086c14cca8f9c4b4c2a105b647a
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2023-10-19 12:37:43 -0600

    Update Spacing testapp.py

--- testapp.py
+++ testapp.py
@@ -8,20 +8,18 @@ Purpose: To Build a Streamlit App That Converts Celsius, Kelvin, and Farenheit t
 
 '''
 
-
 #Import Streamlit
 import streamlit as st
-
 
 #Headers
 st.title("Temperature Convertor App")
 st.header("*By Joti Gokaraju*")
 st.divider()
 
-
 #Conversion Function. Takes Variables and Determines How to Convert Accordingly. Returns Calculated Result Value
 def convert(start_type, start_temp, conversion_type):
-    
+
+    #Celsius Conversions
     if start_type == "Celsius":
         if conversion_type == "Celsius":
             return start_temp
@@ -29,7 +27,8 @@ def convert(start_type, start_temp, conversion_type):
             return (start_temp*(9/5))+32
         else:
             return start_temp+273.15
-        
+
+    #Farenheit Conversions
     elif start_type == "Farenheit":
         if conversion_type == "Celsius":
             return (start_temp-32)*5/9
@@ -37,7 +36,8 @@ def convert(start_type, start_temp, conversion_type):
             return start_temp
         else:
             return ((start_temp-32)*5/9)+273.15
-        
+            
+    #Kelvin Conversions
     else:
         if conversion_type == "Celsius":
             return start_temp-273.15
@@ -45,19 +45,15 @@ def convert(start_type, start_temp, conversion_type):
             return ((start_temp-273.15)*(9/5))+32
         else:
             return start_temp
- 
 
-#User Input Variables
-    
+#User Input Variables 
 #Selectbox Variable to Determine Starting Temperature
 start_type = st.selectbox(
     "What is the Starting Temperature Unit?",
    ("Celsius", "Farenheit", "Kelvin"),
    index=None,
    placeholder="Select Starting Temperature...")
-
 st.write("You Selected:", start_type)
-
 
 #User Inputs the Temperature Value They Want Converted
 start_temp = st.number_input("What is the Current Temperature?", value=None, placeholder="Type the Starting Temperature...")
@@ -69,13 +65,10 @@ conversion_type = st.selectbox(
    ("Celsius", "Farenheit", "Kelvin"),
    index=None,
    placeholder="Select Conversion Temperature...")
-
 st.write('You Selected:', conversion_type)
-
 
 #Divider
 st.divider()
-
 
 #Button to Intitiate Conversion
 if st.button("Convert!", type="primary"):
